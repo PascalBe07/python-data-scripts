4c6681829f5499870475b2f1dbc14d14b93ba784
Pascal Betting <pascal.betting@gmx.de>
2019-07-07 18:42:52 +0200

    fix(time changes): make script produce working formulars; style(time changes): more code formatting; chore(time changes): remove debugging code

--- excel/time-changes.py
+++ excel/time-changes.py
@@ -1,56 +1,38 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from openpyxl import load_workbook
+from openpyxl import load_workbook, utils
 import warnings
 
 
 def createFullCoordinate(cell, sheet):
-    return "$'" + sheet.title + "'." + cell.coordinate
+    return f"{utils.quote_sheetname(sheet.title)}!{cell.coordinate}"
 
 
-def calculateVariableDifference(relevantVariableCells, dataRow, variableName, firstTimeSpot, lastTimeSpot):
+def calculateVariableDifference(relevantVariableCells, dataRow, variableName, firstTimeSpot, lastTimeSpot, targetCellAbsolute):
     firstHeaderCell = list(filter(lambda x: str.startswith(x.value, firstTimeSpot + "_")
                                   and variableName in x.value, relevantVariableCells))[0]
     lastHeaderCell = list(filter(lambda x: str.startswith(x.value, lastTimeSpot + "_")
                                  and variableName in x.value, relevantVariableCells))[0]
     firstDataCell = list(dataRow)[firstHeaderCell.column-1]
     lastDataCell = list(dataRow)[lastHeaderCell.column-1]
-    absolutDifference = "=" + createFullCoordinate(lastDataCell, readingWorksheet) + \
-        " - " + createFullCoordinate(firstDataCell, readingWorksheet)
-    relativeDifference = "=" + createFullCoordinate(firstDataCell, readingWorksheet) + \
-        " / " + createFullCoordinate(lastDataCell, readingWorksheet)
-
-    # print('##########################################################')
-    # print('Printing information about the calculated cell differences')
-    # print('Variable Name:', variableName)
-    # print('Start TimeSpot:', firstTimeSpot)
-    # print('End TimeSpot:', lastTimeSpot)
-    # print('Value of the start data cell:', firstDataCellValue)
-    # print('Value of the last data cell:', lastDataCellValue)
-    # print('Absolute difference', absolutDifference)
-    # print('Relative difference', relativeDifference, 'In percentage:',
-    #       str(round(relativeDifference * 100, 2)) + "%")
-    # print('##########################################################')
+    absolutDifference = f"={createFullCoordinate(lastDataCell, readingWorksheet)}-{createFullCoordinate(firstDataCell, readingWorksheet)}"
+    relativeDifference = f"={createFullCoordinate(targetCellAbsolute, writingWorksheet)}/{createFullCoordinate(firstDataCell, readingWorksheet)}"
     return (absolutDifference, relativeDifference)
 
 
 # arguments of the script
 parser = ArgumentParser()
-parser.add_argument("-f", "--file", dest="filename",
-                    help="file to read", metavar="FILENAME", default="testfile.xlsx")
-parser.add_argument("-s", "--sheetname", dest="sheetname",
-                    help="sheet to read", metavar="SHEETNAME")
+parser.add_argument("-f", "--file", dest="filename", help="file to read", metavar="FILENAME", default="testfile.xlsx")
+parser.add_argument("-s", "--sheetname", dest="sheetname", help="sheet to read", metavar="SHEETNAME")
 args = parser.parse_args()
 sheetname = args.sheetname
 filename = args.filename
-timespots = [
-    'Pre', 'Mid', 'Post']
+timespots = ['Pre', 'Mid', 'Post']
 
 # get excel sheet to read from
 workbook = load_workbook(filename, data_only=True)
-readingWorksheet = workbook[workbook.sheetnames[0]
-                            ] if sheetname == None else workbook[sheetname]
+readingWorksheet = workbook[workbook.sheetnames[0]] if sheetname == None else workbook[sheetname]
 
 # create excel sheet to write to
 newWorksheetName = "Changes-over-time"
@@ -94,17 +76,18 @@ for (variableIndex, variableName) in enumerate(relevantVariableNames):
     firstColumnNumber = 2 + (variableIndex * 2)
     absoluteHeaderCell = writingWorksheet.cell(row=1, column=firstColumnNumber)
     relativeHeaderCell = writingWorksheet.cell(row=1, column=firstColumnNumber + 1)
-    absoluteHeaderCell.value = "Absolute-" + firstTimeSpot + "-" + lastTimeSpot + "-" + variableName
-    relativeHeaderCell.value = "Relative-" + firstTimeSpot + "-" + lastTimeSpot + "-" + variableName
+    absoluteHeaderCell.value = f"Absolute-{firstTimeSpot}-{lastTimeSpot}-{variableName}"
+    relativeHeaderCell.value = f"Relative-{firstTimeSpot}-{lastTimeSpot}-{variableName}"
     absoluteHeaderCell.style = 'Headline 3'
     relativeHeaderCell.style = 'Headline 3'
 
     # go over all rows, calculate differences between values for the specific variable
     # and finally write the difference values to extra columns, whose headers were already written
     for (rowIndex, row) in enumerate(rowList[1:]):
-        difference = calculateVariableDifference(relevantVariableCells, row, variableName, firstTimeSpot, lastTimeSpot)
         absoluteDataCell = writingWorksheet.cell(row=rowIndex + 2, column=firstColumnNumber)
         relativeDataCell = writingWorksheet.cell(row=rowIndex + 2, column=firstColumnNumber + 1)
+        difference = calculateVariableDifference(
+            relevantVariableCells, row, variableName, firstTimeSpot, lastTimeSpot, absoluteDataCell)
         absoluteDataCell.value = difference[0]
         relativeDataCell.value = difference[1]
         absoluteDataCell.style = '20 % - Accent1'
