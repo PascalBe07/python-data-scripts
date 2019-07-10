28c5ce12d267bf4a786fa8a0b7052812221eb6b7
Pascal Betting <pascal.betting@smartinsurtech.de>
2019-07-10 14:53:29 +0200

    feat: finish merge request script

--- gitlab/create-merge-request.py
+++ gitlab/create-merge-request.py
@@ -10,9 +10,22 @@ parser.add_argument("-b", "--source-branch", dest="sourcebranch",
                     help="The branch to create a merge request from", required=True)
 parser.add_argument("-a", "--auth-token", dest="token",
                     help="The personal access token for Gitlab", required=True)
-parser.add_argument("-t", "--title", dest="token",
+parser.add_argument("-t", "--title", dest="title",
                     help="The title of the merge request", required=True)
+parser.add_argument("-p", "--project", dest="project",
+                    help="The project for which we want to create a MR", required=True)
 args = parser.parse_args()
 
-response = requests.get(
-    'https://' + args.servername + '/api/v4/merge_requests')
+projectId = 'nkk%2F' + args.project
+endpoint = 'http://' + args.servername + '/api/v4/projects/' + projectId + '/merge_requests'
+header = { 'PRIVATE-TOKEN': args.token }
+jsonData = {
+    'id': projectId,
+    'source_branch': args.sourcebranch,
+    'target_branch': 'master',
+    'title': args.title,
+    'remove_source_branch': 'true'
+}
+response = requests.post(endpoint, json=jsonData, headers=header)
+print(response.status_code)
+print(response.json())
