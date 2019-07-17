35ef4f8672603d9b4329163f830d7921ea3a0e97
Pascal Betting <pascal.betting@gmx.de>
2019-07-17 22:04:42 +0200

    feat(excel - changes chart): add data to graph; use colors of data to group data

--- excel/changes-chart.py
+++ excel/changes-chart.py
@@ -38,22 +38,34 @@ headerRow = rowList[0][1:]
 dataRow = rowList[sourceRowNumber-1][1:]
 columnNumber = len(headerRow)
 
+# write row headers
+writingWorksheet.cell(1, 1, 'Point in Time')
+writingWorksheet.cell(2, 1, 'Pre')
+writingWorksheet.cell(3, 1, 'Post')
 
 # write values for chart to a sheet, so that table can reference these values
 for (index, column) in enumerate(headerRow):
-    writingWorksheet.cell(1, index+1, column.value)
-    writingWorksheet.cell(2, index+1, 0)
-    writingWorksheet.cell(3, index+1, dataRow[index].value)
+    writingWorksheet.cell(1, index+2, column.value)
+    writingWorksheet.cell(2, index+2, 0)
+    writingWorksheet.cell(3, index+2, dataRow[index].value)
 
 
-chart = LineChart()
-chart.title = "Relative Changes as chart"
-chart.style = 13
-chart.y_axis.title = "Relative Change"
-chart.x_axis.title = "Time"
-chartData = Reference(writingWorksheet, min_col=1, min_row=1, max_col=columnNumber+1, max_row=3)
+c1 = LineChart()
+c1.title = "Relative Changes as chart"
+c1.style = 13
+c1.y_axis.title = "Relative Change"
+c1.x_axis.title = "Time"
+data = Reference(writingWorksheet, min_col=2, min_row=1, max_col=columnNumber+1, max_row=3)
+c1.add_data(data, titles_from_data=True)
 
-writingWorksheet.add_chart(chart, "A5")
 
+# color each line within the chart looking at the value
+for (index, column) in enumerate(headerRow):
+    line = c1.series[index]
+    line.graphicalProperties.line.width = 100
+    color = column.fill.start_color.index
+    line.graphicalProperties.line.solidFill = color
+
+writingWorksheet.add_chart(c1, "A5")
 
 workbook.save(filename)
